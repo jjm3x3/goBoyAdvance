f366379dd4531d62a6e7be3b34578abe77c112b8
jjm3x3 <jjm3333@gmail.com>
2016-07-24 12:03:50 -0500

    Add ability to execute

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 	//"encoding/hex" //not sure if I should even use this
 )
 
+type Z80 struct {
+	reg_sp int16
+	reg_pc int
+}
+
+type SystemState struct {
+	bootRom []byte
+}
+
+var (
+	copc  Z80
+	state SystemState
+)
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -23,18 +37,28 @@ func main() {
 	stringBytes, err := bufferedReader.ReadString('P')
 	check(err)
 
-	realBytes := []byte(stringBytes)
+	state.bootRom = []byte(stringBytes)
 
-	twoByte := getXbytePair(realBytes, 0)
-
-	fmt.Printf("this is 1 + 2: %x\n", twoByte)
-
-	if twoByte == 0x31fe {
-		fmt.Println("now were in business")
+	for copc.reg_pc = 0; copc.reg_pc < len(state.bootRom); copc.reg_pc++ {
+		byte := getXByte(copc.reg_pc)
+		execute(byte)
 	}
-
 }
 
-func getXbytePair(byteslice []byte, pos int) int {
-	return int(byteslice[0*2])*256 + int(byteslice[1*2])
+func execute(byte int16) {
+	if byte == 0x11 || byte == 0x21 || byte == 0x31 || byte == 0x41 {
+		fmt.Printf("this is ld dd,nn (load imidate)")
+		copc.reg_pc++
+		secondPart := getXByte(copc.reg_pc)
+		copc.reg_pc++
+		firstPart := getXByte(copc.reg_pc)
+		fmt.Printf("loading this address: %x %x\n", firstPart, secondPart)
+	} else {
+		fmt.Printf("this is: %x\n", byte)
+	}
+}
+
+func getXByte(pos int) int16 {
+	oneByte := int16(state.bootRom[pos])
+	return oneByte
 }
